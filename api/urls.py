858a9000cd967813c1f79db71f2f63693846be9a
Sam Logsdon <slogsdon@fastmail.fm>
2018-09-26 19:07:36 -0400

    -Added OpenAPI documentation endpoint (view at /swagger) -Registered ViewSets with DRF router

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,32 @@
 from django.contrib.auth import get_user_model
 from django.urls import path, re_path, include
 from djoser.views import UserViewSet
-from rest_framework import routers
-router = routers.DefaultRouter()
-urlpatterns = router.urls
+from api.views import EmojiViewSet, MessageViewSet, ResponseViewSet, ScheduleViewSet
+from rest_framework import routers, permissions
+from rest_framework.schemas import get_schema_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Emoji-Backend API",
+      default_version='v1',
+      description="Work in progress.",
+   ),
+   validators=['flex', 'ssv'],
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+router = routers.DefaultRouter()
+router.register('emoji',EmojiViewSet)
+router.register('messages',MessageViewSet)
+router.register('responses', ResponseViewSet)
+router.register('schedule', ScheduleViewSet)
+urlpatterns = router.urls
 urlpatterns += [
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
