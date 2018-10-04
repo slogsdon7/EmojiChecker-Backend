489fb96d4ec131ffc3ef81ec259d014b99d5eba8
Sam Logsdon <slogsdon@fastmail.fm>
2018-10-04 14:56:54 -0400

    * added help text to send_at field * added a OneToOneField for responses to SendLog model * SMS class no longer tries to create a client so that tests will run * Added response

--- api/sms.py
+++ api/sms.py
@@ -3,7 +3,7 @@ import boto3
 class SMS():
     def __init__(self,  message):
         self.message = message
-        self.client = boto3.client('sns')
+        # self.client = boto3.client('sns')
 
     def send(self, phone_number):
         """for now returns only returns True for the sake of writing code that uses this command"""
